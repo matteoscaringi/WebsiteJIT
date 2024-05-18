0096dbe1de998e25b194d415d51282651719c4b7
matteoscaringi <me@matteoscaringi.be>
2024-05-18 14:40:58 +0200

    changed corlors and fixed burger menu

--- WebsiteJIT-Domain/Business/Controller.cs
+++ WebsiteJIT-Domain/Business/Controller.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebsiteJIT_Domain.Persistence;
+using WebsiteJIT_Domain.Business.Classes;
 
 namespace WebsiteJIT_Domain.Business
 {
